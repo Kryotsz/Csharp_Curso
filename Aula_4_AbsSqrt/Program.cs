83996e0bbd72cdd3e5b7eae34522ed355edaca14
Kryotsz <104468900+Kryotsz@users.noreply.github.com>
2022-04-26 19:48:41 -0300

    Add files via upload

---
+++ Aula_4_AbsSqrt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula_4_AbsSqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, raiz;
+
+            Console.Write("Informe um valor: ");
+
+            valor=double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            raiz=Math.Sqrt(valor);
+
+            Console.WriteLine("Raiz quadrade de "+valor+": "+raiz);
+        }
+    }
+}
