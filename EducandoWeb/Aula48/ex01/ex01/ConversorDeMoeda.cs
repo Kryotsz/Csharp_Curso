00967fbbfdbd62386fe551648b263c773eaa053f
Igor <igorhgassen@gmail.com>
2023-09-12 15:06:27 -0300

    Exercício de membros estáticos adicionado

---
+++ EducandoWeb/Aula48/ex01/ex01/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace ex01
+{
+    internal class ConversorDeMoeda
+    {
+        public static decimal Iof = 0.06M;
+
+        public static decimal Conversao(decimal cotacao, decimal valor)
+        {
+            return valor * cotacao + (valor * cotacao) * Iof;
+        }
+    }
+}
---
+++ EducandoWeb/Aula48/ex01/ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿using ex01;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dólar? ");
+decimal cotacao = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dólares você vai comprar? ");
+decimal valor = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+decimal valorPago = ConversorDeMoeda.Conversao(cotacao, valor);
+
+Console.WriteLine("Valor a ser pago em reais = " + valorPago.ToString("F2", CultureInfo.InvariantCulture));
