68e98c56129a2ad1623a22c4db0eddae59e2c920
Igor <igorhgassen@gmail.com>
2023-09-12 11:57:38 -0300

    Exercício atualizado

--- EducandoWeb/Aula42/exProduto/exProduto/Produto.cs
+++ EducandoWeb/Aula42/exProduto/exProduto/Produto.cs
@@ -13,6 +13,16 @@ namespace exProduto
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
         public override string ToString()
         {
             return Nome 
--- EducandoWeb/Aula42/exProduto/exProduto/Program.cs
+++ EducandoWeb/Aula42/exProduto/exProduto/Program.cs
@@ -11,4 +11,23 @@ p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 Console.Write("Quantidade no estoque: ");
 p.Quantidade = int.Parse(Console.ReadLine());
 
+Console.WriteLine();
 Console.WriteLine($"Dados do produto: {p}");
+
+Console.WriteLine();
+Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+int qtde = int.Parse(Console.ReadLine());
+
+p.AdicionarProdutos(qtde);
+
+Console.WriteLine();
+Console.WriteLine($"Dados atualizados: {p}");
+
+Console.WriteLine();
+Console.Write($"Digite o número de produtos a ser removido do estoque: ");
+qtde = int.Parse(Console.ReadLine());
+
+p.RemoverProdutos(qtde);
+
+Console.WriteLine();
+Console.WriteLine($"Dados atualizados: {p}");
