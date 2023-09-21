45516d6fc11e40c2782a5f09ff3694741510ea44
Igor <igorhgassen@gmail.com>
2023-09-21 16:58:34 -0300

    Aulas adicionadas

---
+++ EducandoWeb/Aula85/Var/Var/Program.cs
@@ -0,0 +1,6 @@
+﻿var x = 10;
+var y = 20.0;
+var z = "Maria";
+
+var w = z;
+//int w = z;
---
+++ EducandoWeb/Aula87/CondicionalTernaria/CondicionalTernaria/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double desconto;
+
+// If e Else normal
+if (preco < 20.0)
+{
+    desconto = preco * 0.1;
+}
+else
+{
+    desconto = preco * 0.05;
+}
+
+Console.WriteLine(desconto);
+
+// If ternário (Expressão Condicional Ternária)
+desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+Console.WriteLine(desconto);
