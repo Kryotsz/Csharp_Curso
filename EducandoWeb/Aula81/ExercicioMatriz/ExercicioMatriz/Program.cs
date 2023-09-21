2d62cf12bd01a5fd4a63f55b90a69006c840144e
Igor <igorhgassen@gmail.com>
2023-09-21 16:27:22 -0300

    Exercício de Fixação de Matrizes

---
+++ EducandoWeb/Aula81/ExercicioMatriz/ExercicioMatriz/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] inteiros = Console.ReadLine().Split(' ');
+int m = int.Parse(inteiros[0]);
+int n = int.Parse(inteiros[1]);
+
+int[,] mat = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    string[] linha = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n; j++)
+    {
+        mat[i,j] = int.Parse(linha[j]);
+    }
+}
+
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (mat[i,j] == x)
+        {
+            Console.WriteLine($"Position {i},{j}:");
+            if (j > 0)
+            {
+                Console.WriteLine($"Left: {mat[i,(j-1)]}");
+            }
+            if (i > 0)
+            {
+                Console.WriteLine($"Up: {mat[(i-1),j]}");
+            }
+            if (j < (n-1))
+            {
+                Console.WriteLine($"Right: {mat[i,(j+1)]}");
+            }
+            if (i < (m-1))
+            {
+                Console.WriteLine($"Down: {mat[(i+1),j]}");
+            }
+        }
+    }
+}
