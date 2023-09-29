878adbdff8db14365b3cfe7eaeb6cef1e65e5b10
Igor <igorhgassen@gmail.com>
2023-09-29 13:35:28 -0300

    Exercício Resolvido adicionado

---
+++ EducandoWeb/Aula140/ExercicioExemploHerancaPolimorfismo/ExercicioExemploHerancaPolimorfismo/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioExemploHerancaPolimorfismo.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionlCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionlCharge;
+        }
+
+        public override double Payment()
+        {
+            return Hours * ValuePerHour + AdditionalCharge * 1.1;
+        }
+    }
+}
---
+++ EducandoWeb/Aula140/ExercicioExemploHerancaPolimorfismo/ExercicioExemploHerancaPolimorfismo/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExercicioExemploHerancaPolimorfismo.Entities;
+using System.Globalization;
+
+List<Employee> employees = new List<Employee>();
+
+Console.Write("Enter the number of employees: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Employee #{i} data:");
+    Console.Write("Outsourced (y/n)? ");
+    char outsourced = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if (outsourced == 'y')
+    {
+        Console.Write("Additional charge: ");
+        double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        employees.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+    }
+    else
+    {
+        employees.Add(new Employee(name, hours, valuePerHour));
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("PAYMENTS:");
+foreach (Employee emp in employees)
+{
+    Console.WriteLine(emp);
+}
