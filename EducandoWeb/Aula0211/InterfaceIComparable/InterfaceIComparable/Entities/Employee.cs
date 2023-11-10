740713071380ddd42a25575ec741300dc249a9f0
Igor Gassen <igorhgassen@gmail.com>
2023-11-10 10:42:32 -0300

    Aula adicionada sobre IComparable

---
+++ EducandoWeb/Aula0211/InterfaceIComparable/InterfaceIComparable/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace InterfaceIComparable.Entities
+{
+    internal class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        // construtor recebe a linha do arquivo csv
+        public Employee(string csvEmployee)
+        {
+            // separa a linha onde tem vírgula e coloca em um array
+            string[] vetor = csvEmployee.Split(',');
+            // separa o array em nome e salário
+            Name = vetor[0];
+            Salary = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Employee.");
+            }
+
+            // Downcasting
+            Employee other = obj as Employee;
+            // compara o nome do atual funcionário com o recebido por parâmetro através do list.Sort()
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ EducandoWeb/Aula0211/InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,26 @@
+﻿using InterfaceIComparable.Entities;
+
+string path = @"C:\Users\Igor\Desktop\Igor\Repositorios\Csharp_Curso\EducandoWeb\Arquivos para Testes\aula0211.csv";
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        List<Employee> list = new List<Employee>();
+        while (!sr.EndOfStream)
+        {
+            list.Add(new Employee(sr.ReadLine()));
+        }
+        // list.Sort() implementa a interface IComparable naturalmente
+        list.Sort();
+        foreach (Employee emp in list)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred!");
+    Console.WriteLine(e.Message);
+}
