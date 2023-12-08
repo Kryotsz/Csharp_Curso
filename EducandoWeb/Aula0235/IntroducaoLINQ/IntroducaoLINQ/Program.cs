42ae82041788874a4e473120e9b822652b6fc49d
Igor <igorhgassen@gmail.com>
2023-12-08 14:35:08 -0300

    Aula introdutória ao LINQ adicionada

---
+++ EducandoWeb/Aula0235/IntroducaoLINQ/IntroducaoLINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Specify the data source
+        int[] numbers = new int[] { 2, 3, 4, 5 };
+
+        // Define the query expression
+        // vai gerar uma nova coleção, somente com os itens que atenderem o predicado
+        // retorna um IEnumerable
+        var result = numbers
+            .Where(x => x % 2 == 0)
+            .Select(x => x * 10);
+        // nesse exemplo, recebe x, tal que o resto da divisão de x por 2 é igual a 0
+        // e recebe x e faz x * 10
+        // ou seja, pega todos os elementos pares da fonte de dados e multiplica por 10
+
+        // Execute the query
+        foreach (int x in result)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
